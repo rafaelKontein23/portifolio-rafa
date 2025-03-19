aa7ed3afd6a690d25e0de1635c7a7c72f36b7524
rafael.tein <rafael.tein@visaogrupo.com.br>
2025-03-19 18:28:00 -0300

    ajuste de itens]

--- src/components/inicial/InicialHome.tsx
+++ src/components/inicial/InicialHome.tsx
@@ -5,37 +5,47 @@ import android from '/android.png'
 
 function InicialHome() {
     return (
-        <div className='flex justify-between items-center px-16'>
-            <div className='w-1/2 pt-[150px] ml-[130px]'>
-                <div className='flex h-10 items-center justify-start bg-[#272020] px-4 py-2 w-[175px] rounded-[100px]'>
-                    <div className='rounded-[100px] bg-[#14DC49] h-[8px] w-[8px] mr-2'></div>
-                    <span className='text-[16px] opacity-50 font-[500] tracking-[0.15px] font-jetbrains'>Java & Kotlin</span>
+        <>
+            <div className='relative flex justify-between items-center min-h-svh container mx-auto'>
+                <div className='w-1/2'>
+                    <div className='flex h-10 items-center justify-start bg-[#272020] px-4 py-2 w-[175px] rounded-[100px]'>
+                        <div className='rounded-[100px] bg-[#14DC49] h-[8px] w-[8px] mr-2'></div>
+                        <span className='text-[16px] opacity-50 font-[500] tracking-[0.15px] font-jetbrains'>Java & Kotlin</span>
+                    </div>
+
+                    <div className='mt-[40px]'>
+                        <h2 className='text-[64px] font-jetbrains tracking-[0.15px]'>
+                            Desenvolvimento de
+                            <span className='border-b-12 border-solid border-b-[#DC143C] mb-[-20px]'> Software</span> de Alta
+                            Performance
+                        </h2>
+                        <p className='text-[24px] opacity-50 font-[500] tracking-[0.15px] mt-[40px]'>
+                            Especializado em desenvolvimento Android e aplicações empresariais com Java e Kotlin
+                        </p>
+                    </div>
+                    <div className='flex justify-start'>
+
+                        <button className='flex bg-[#DC143C] w-[220px] h-[48px] p-2 mt-[40px] w-1/2  justify-center place-items-center'>
+                            <span className='font-jetbrains '>Ver Projetos</span>
+                            <img src={seta_baixo} />
+                        </button>
+                        <h1 className='text-8x1 place-content-center ml-12'>|</h1>
+
+                        <span className='place-content-center p-12 font-jetbrains'>Entre em contato</span>
+
+                    </div>
+
+
+
                 </div>
 
-                <div className='mt-[40px]'>
-                    <h2 className='text-[64px] font-jetbrains tracking-[0.15px]'>
-                        Desenvolvimento de
-                        <span className='border-b-12 border-solid border-b-[#DC143C] mb-[-20px]'> Software</span> de Alta
-                        Performance
-                    </h2>
-                    <p className='text-[24px] opacity-50 font-[500] tracking-[0.15px] mt-[40px]'>
-                        Especializado em desenvolvimento Android e aplicações empresariais com Java e Kotlin
-                    </p>
+                <div className="w-1/2 flex flex-col justify-between items-center self-stretch h-full">
+                    <img className='absolute top-[232px] right-0' src={iphone_image} alt="iPhone" />
                 </div>
-                <div className='flex justify-start'>
+            </div>
 
-                    <button className='flex bg-[#DC143C] w-[220px] h-[48px] p-2 mt-[40px] w-1/2  justify-center place-items-center'>
-                        <span className='font-jetbrains '>Ver Projetos</span>
-                        <img src={seta_baixo} />
-                    </button>
-                    <h1 className='text-8x1 place-content-center ml-12'>|</h1>
-
-                    <span className='place-content-center p-12 font-jetbrains'>Entre em contato</span>
-
-                </div>
-
-
-                <section className='w-4xl'>
+            <section className='container relative mx-auto flex items-center justify-center gap-40 '>
+                <div className='flex-1'>
                     <h1 className='font-jetbrains mt-[200px] text-[48px]'>Desenvolvedor apaixonado por tecnologia</h1>
 
                     <p className='text-[24px] opacity-50 max-w-5x1 font-[500] tracking-[0.15px] text-left mt-[40px] text-sm/14'>
@@ -46,15 +56,30 @@ function InicialHome() {
                         Já desenvolvi projetos em React, incluindo este próprio portfólio. Embora não seja minha especialidade, consigo construir aplicações funcionais e bem estruturadas quando necessário.
 
                         Estou sempre em busca de novos desafios e oportunidades para aprimorar minhas habilidades e entregar soluções de alta qualidade! 🚀                    </p>
-                </section>
+                </div>
 
-            </div>
+                <div className=' w-[385px] '>
+                    <img className='absolute bottom-0 right-0' src={android} />
+                </div>
 
-            <div className="w-1/2 flex flex-col justify-between items-center self-stretch h-full">
-                <img className="mt-64" src={iphone_image} alt="iPhone" />
-                <img className="m" src={android} alt="Android" />
-            </div>
-        </div>
+            </section>
+
+            <section className='container mx-auto '>
+                <h1 className='font-jetbrains mt-[200px]  text-[48px]'>Projetos desenvolvidos</h1>
+
+                <div className='mt-3 flex gap-[32px]'>
+                    <p className='border-[#F5EEED] border-[1px] pt-[12px] pb-[12px] pr-[24px] pl-[24px] text-[#F5EEED] w-[95px] hover:border-[#DC143C] hover:text-[#DC143C]'>Todos</p>
+                    <p className='border-[#F5EEED] border-[1px] pt-[12px] pb-[12px] pr-[24px] pl-[24px] text-[#F5EEED] w-[120px] hover:border-[#DC143C] hover:text-[#DC143C]'>Back-end</p>
+                    <p className='border-[#F5EEED] border-[1px] pt-[12px] pb-[12px] pr-[24px] pl-[24px] text-[#F5EEED] w-[95px] hover:border-[#DC143C] hover:text-[#DC143C]'>Andoid</p>
+
+
+
+
+                </div>
+
+
+            </section>
+        </>
     )
 }
 
--- src/components/menu/menuCima.tsx
+++ src/components/menu/menuCima.tsx
@@ -26,21 +26,24 @@ function MenuCima() {
   ]
 
   return (
-    <nav className="flex items-center justify-between px-48 bg-[#1D181A] w-full top-0 left-0 z-[1000] h-[62px] border-b border-solid border-b-[#403534]">
-      <div className="logo">
-        <span className="font-jetbrains text-[24px]">Rafael<strong className="text-[#DC143C] font-jetbrains">.</strong>Kon</span>
+    <nav className=" bg-[#1D181A] w-full top-0 left-0 z-[1000]  border-b border-solid border-b-[#403534]">
+      <div className='flex items-center justify-between  h-[62px] container mx-auto'>
+        <div className="logo">
+          <span className="font-jetbrains text-[24px]">Rafael<strong className="text-[#DC143C] font-jetbrains">.</strong>Kon</span>
+        </div>
+
+        <div className="menu">
+          <ul className="flex items-end gap-16">
+            {menu.map((m, i) => (
+              <li key={i}>
+                <a href={m.link} className='h-[62px] border-b-4 border-b-transparent flex items-center justify-center transition-colors duration-300 hover:border-b-[#DC143C]'>{m.nome}</a>
+              </li>
+            ))}
+
+          </ul>
+        </div>
       </div>
 
-      <div className="menu">
-        <ul className="flex items-end gap-16">
-          {menu.map((m, i) => (
-            <li key={i}>
-              <a href={m.link} className='h-[62px] border-b-4 border-b-transparent flex items-center justify-center transition-colors duration-300 hover:border-b-[#DC143C]'>{m.nome}</a>
-            </li>
-          ))}
-
-        </ul>
-      </div>
 
     </nav>
 
