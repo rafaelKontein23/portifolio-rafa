402600edd883bfc46fbe06c9ecd5caeb67b9e6c6
rafael.tein <rafael.tein@visaogrupo.com.br>
2025-03-27 10:39:54 -0300

    ajuste de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { tr } from 'framer-motion/client'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
   ],
-  base:"/portifolio",
+  base:"/portifolio-rafa",
   server: {
      open:true
   }
